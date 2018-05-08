6b1acfdd3e84bfda569e0572b61983477fe5eec1
g.sanchez <nexus.gsan@gmail.com>
2018-05-08 00:47:06 -0500

    estructura base del proyecto, librerias base, archivos base, todo base, ja ja ja

--- MariaItzel/settings.py
+++ MariaItzel/settings.py
@@ -128,7 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files
 
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join('../users/static')
 
 admin.site.site_header = 'Sistema Mariaitzel'
 admin.site.site_title = 'Mariaitzel'
